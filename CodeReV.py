3e76f5213ca9bd2a603e5a6569d0b1dcf15fc3e0
jinsub <jinsub1113@naver.com>
2023-11-22 15:18:43 +0900

    change1

--- CodeReV.py
+++ CodeReV.py
@@ -1,16 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-np.random.seed(0); data = np.random.randn(100, 2)
+
+np.random.seed(0)
+data = np.random.randn(100, 2)
+
+# Create subplots
 fig, axes = plt.subplots(2, 2, figsize=(12, 10))
-a = data[:, 0]; b = data[:, 1]; axes[0, 0].bar(['Mean', 'Median'], [np.mean(a), np.median(a)],
-color='blue', alpha=0.7); axes[0, 0].bar(['Mean', 'Median'], [np.mean(b), np.median(b)],
-color='green', alpha=0.7); axes[0, 0].legend(); axes[0, 0].set_title('Descriptive Statistics:
-Mean and Median')
-sns.heatmap(np.corrcoef(data.T), annot=True, ax=axes[0, 1]); axes[0,
-1].set_title('Correlation Analysis'); axes[1, 0].hist(a, bins=15, color='blue', alpha=0.7);
-axes[1, 0].hist(b, bins=15, color='green', alpha=0.7); axes[1, 0].legend(); axes[1,
-0].set_title('Histogram of Variables')
-axes[1, 1].scatter(a, b, alpha=0.7); axes[1, 1].set_xlabel('Variable 1'); axes[1,
-1].set_ylabel('Variable 2'); axes[1, 1].set_title('Scatter Plot of Variable 1 vs Variable 2')
-plt.tight_layout(); plt.show()
+
+# Bar plot for mean and median
+variables = ['Variable 1', 'Variable 2']
+for i, variable in enumerate([data[:, 0], data[:, 1]]):
+    axes[0, 0].bar(['Mean', 'Median'], [np.mean(variable), np.median(variable)],
+                   color=sns.color_palette('deep')[i], alpha=0.7, label=variables[i])
+
+axes[0, 0].legend()
+axes[0, 0].set_title('Descriptive Statistics: Mean and Median')
+
+# Heatmap for correlation analysis
+sns.heatmap(np.corrcoef(data.T), annot=True, ax=axes[0, 1])
+axes[0, 1].set_title('Correlation Analysis')
+
+# Histograms of variables
+for i, variable in enumerate([data[:, 0], data[:, 1]]):
+    sns.histplot(variable, bins=15, color=sns.color_palette('deep')[i], alpha=0.7, ax=axes[1, 0])
+
+axes[1, 0].set_title('Histogram of Variables')
+
+# Scatter plot
+axes[1, 1].scatter(data[:, 0], data[:, 1], alpha=0.7)
+axes[1, 1].set_xlabel('Variable 1')
+axes[1, 1].set_ylabel('Variable 2')
+axes[1, 1].set_title('Scatter Plot of Variable 1 vs Variable 2')
+
+# Adjust layout and show the plot
+plt.tight_layout()
+plt.show()
